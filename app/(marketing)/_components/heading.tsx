5385aeab1a1e260cd5ca44531b5d8ed9b44fa946
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-12 22:54:09 +0530

    heroes heading and images

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,12 +1,23 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
 export const Heading = () => {
 	return (
 		<div className="max-w-3xl space-y-4">
 			<h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-				Your Ideas, Documents & Plans. Unified. Welcome to
+				Your Ideas, Documents & <br /> Plans. Unified. Welcome <br /> to{" "}
 				<span className="underline">Clotion</span>
 			</h1>
+			<h3 className="text-base sm:text-xl md:text-2xl font-medium">
+				Clotion is the connected workspace where <br />
+				better, faster work happens.
+			</h3>
+			<Button>
+				Enter Clotion
+				<ArrowRight className="h-4 w-4 ml-2" />
+			</Button>
 		</div>
 	);
 };
