8f9a2535c194710933a6f5f2eaeb92338b4d0fff
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-16 14:49:22 +0530

    fixed navbar bug

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -86,7 +86,7 @@ const Navigation = () => {
 					isMobile && "left-0 w-full"
 				)}
 			>
-				<nav className="bg-transparent px-3 py-2 w-full">
+				<nav ref={navbarRef} className="bg-transparent px-3 py-2 w-full">
 					{isCollapsed && (
 						<MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />
 					)}
