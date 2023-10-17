12840e75d0c43052e638bda2061214c3826e3e3b
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-17 23:20:32 +0530

    search filter and document blocks

--- app/(main)/_components/trash-can.tsx
+++ app/(main)/_components/trash-can.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
+import { Input } from "@/components/ui/input";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
+import { Search } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
@@ -52,5 +55,38 @@ export const TrashCan = () => {
 		}
 	};
 
-	return <div>trash can</div>;
+	if (documents === undefined) {
+		<div className="h-full flex items-center justify-center">
+			<Spinner size="lg" />
+		</div>;
+	}
+
+	return (
+		<div className="text-sm">
+			<div className="flex items-center gap-x-1 p-2">
+				<Search className="h-4 w-4" />
+				<Input
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+					className="h-7 px-2 focus-visible:ring-transparent bg-secondary"
+					placeholder="Filter by page title..."
+				/>
+			</div>
+			<div className="mt-2 px-1 pb-1">
+				<p className="hidden last:block text-xs text-center text-muted-foreground pb-2">
+					No documents found.
+				</p>
+				{filteredDocuments?.map((document) => (
+					<div
+						key={document._id}
+						role="button"
+						onClick={() => onClick(document._id)}
+						className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center text-primary justify-between"
+					>
+						<span className="truncate pl-2">{document.title}</span>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
