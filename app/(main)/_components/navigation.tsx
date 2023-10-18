a1156292a420e3c94228008608ff8a7dd03a744a
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-18 16:30:29 +0530

    search functionality, modalprovider

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -26,9 +26,11 @@ import {
 	PopoverTrigger,
 } from "@/components/ui/popover";
 import { TrashCan } from "./trash-can";
+import { useSettings } from "@/hooks/use-settings";
 
 const Navigation = () => {
 	const search = useSearch();
+	const settings = useSettings();
 	const pathname = usePathname();
 	const isMobile = useMediaQuery("(max-width: 768px)");
 	const create = useMutation(api.documents.create);
@@ -147,7 +149,7 @@ const Navigation = () => {
 				<div>
 					<UserItem />
 					<Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
-					<Item label="Settings" icon={Settings} onClick={() => {}} />
+					<Item label="Settings" icon={Settings} onClick={settings.onOpen} />
 					<Item onClick={handleCreate} label="New page" icon={PlusCircle} />
 				</div>
 				<div className="mt-4">
