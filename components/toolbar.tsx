b994c79418422211fd0facd38dad07be3d3184e5
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-19 18:07:18 +0530

    documentpage and toolbar component

---
+++ components/toolbar.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Doc } from "@/convex/_generated/dataModel";
+
+interface ToolbarProps {
+	initialData: Doc<"documents">;
+	preview?: boolean;
+}
+
+export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+	return <div>toolbar</div>;
+};
