ff86af1df9f47f0a01941ac233783027e759515e
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-20 22:59:54 +0530

    change cover functionality

--- components/cover.tsx
+++ components/cover.tsx
@@ -2,6 +2,9 @@
 
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { Button } from "./ui/button";
+import { ImageIcon, X } from "lucide-react";
+import { useCoverImage } from "@/hooks/use-cover-image";
 
 interface CoverProps {
 	url?: string;
@@ -9,6 +12,7 @@ interface CoverProps {
 }
 
 export const Cover = ({ url, preview }: CoverProps) => {
+	const coverImage = useCoverImage();
 	return (
 		<div
 			className={cn(
@@ -18,6 +22,19 @@ export const Cover = ({ url, preview }: CoverProps) => {
 			)}
 		>
 			{!!url && <Image src={url} alt="cover" fill className="object-cover" />}
+			{url && !preview && (
+				<div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+					<Button
+						onClick={coverImage.onOpen}
+						className="text-muted-foreground text-xs"
+						variant="outline"
+						size="sm"
+					>
+						<ImageIcon className="h-4 w-4 mr-2" />
+						Change cover
+					</Button>
+				</div>
+			)}
 		</div>
 	);
 };
