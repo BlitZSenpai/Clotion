a08aec96b8dcb8eacba52a302115fd05336c8f7e
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-21 16:54:05 +0530

    publish pop over and unpublish component

--- app/(main)/_components/publish.tsx
+++ app/(main)/_components/publish.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
 import { api } from "@/convex/_generated/api";
 import { Doc } from "@/convex/_generated/dataModel";
 import { useOrigin } from "@/hooks/use-origin";
 import { useMutation } from "convex/react";
+import { Check, Copy, Globe } from "lucide-react";
 import { useState } from "react";
 import { toast } from "sonner";
 
@@ -59,8 +66,69 @@ export const Publish = ({ initialData }: PublishProps) => {
 	};
 
 	return (
-		<div>
-			<div>Publish</div>
-		</div>
+		<Popover>
+			<PopoverTrigger asChild>
+				<Button variant="ghost" size="sm">
+					Publish
+					{initialData.isPublished && (
+						<Globe className="text-sky-500 animate-pulse w-4 h-4 ml-2" />
+					)}
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="w-72" align="end" alignOffset={8} forceMount>
+				{initialData.isPublished ? (
+					<div className="space-y-4">
+						<div className="flex items-center gap-x-2">
+							<Globe className=" animate-pulse h-4 w-4" />
+							<p className="text-sm font-medium">
+								This note is live on the web.
+							</p>
+						</div>
+						<div className="flex items-center">
+							<input
+								className="flex-1 px-2 text-xs border rounded-l-md h-8 bg-muted truncate"
+								value={url}
+								disabled
+							/>
+							<Button
+								onClick={onCopy}
+								disabled={copied}
+								className="h-8 rounded-l-none"
+							>
+								{copied ? (
+									<Check className="h-4 w-4" />
+								) : (
+									<Copy className="h-4 w-4" />
+								)}
+							</Button>
+						</div>
+						<Button
+							onClick={onUnPublish}
+							disabled={isSubmitting}
+							className="w-full text-xs"
+							size="sm"
+						>
+							Unpublish
+						</Button>
+					</div>
+				) : (
+					<div className="flex flex-col items-center justify-center">
+						<Globe className="h-8 w-8 text-muted-foreground mb-2" />
+						<p className="text-sm font-medium mb-2">Publish this note</p>
+						<span className="text-xs text-muted-foreground mb-4">
+							Share your work with others.
+						</span>
+						<Button
+							disabled={isSubmitting}
+							onClick={onPublish}
+							className="w-full text-xs"
+							size="sm"
+						>
+							Publish
+						</Button>
+					</div>
+				)}
+			</PopoverContent>
+		</Popover>
 	);
 };
