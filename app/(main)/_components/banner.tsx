8adde6d2ea382e5293fc04b64f715fdb76326bf1
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-23 18:20:56 +0530

    main folder format

--- app/(main)/_components/banner.tsx
+++ app/(main)/_components/banner.tsx
@@ -9,55 +9,53 @@ import { Button } from "@/components/ui/button";
 import { ConfirmModal } from "@/components/modals/confirm-modals";
 
 interface BannerProps {
-	documentId: Id<"documents">;
+  documentId: Id<"documents">;
 }
 
 export const Banner = ({ documentId }: BannerProps) => {
-	const router = useRouter();
-	const restore = useMutation(api.documents.restore);
-	const remove = useMutation(api.documents.remove);
+  const router = useRouter();
+  const restore = useMutation(api.documents.restore);
+  const remove = useMutation(api.documents.remove);
 
-	const onRestore = () => {
-		const promise = restore({ id: documentId });
+  const onRestore = () => {
+    const promise = restore({ id: documentId });
 
-		toast.promise(promise, {
-			loading: "Restoring note...",
-			success: "Note restored!",
-			error: "Failed to restore note.",
-		});
-	};
+    toast.promise(promise, {
+      loading: "Restoring note...",
+      success: "Note restored!",
+      error: "Failed to restore note.",
+    });
+  };
 
-	const onRemove = () => {
-		const promise = remove({ id: documentId });
+  const onRemove = () => {
+    const promise = remove({ id: documentId });
 
-		toast.promise(promise, {
-			loading: "Deleting note...",
-			success: "Note deleted!",
-			error: "Failed to delete note.",
-		});
-		router.push("/documents");
-	};
+    toast.promise(promise, {
+      loading: "Deleting note...",
+      success: "Note deleted!",
+      error: "Failed to delete note.",
+    });
+    router.push("/documents");
+  };
 
-	return (
-		<div className="flex items-center justify-center bg-rose-500 text-white w-full text-center text-sm p-2 gap-x-2">
-			<p>This note is in the trash</p>
-			<Button
-				size="sm"
-				onClick={onRestore}
-				variant="outline"
-				className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
-			>
-				Restore page
-			</Button>
-			<ConfirmModal onConfirm={onRemove}>
-				<Button
-					size="sm"
-					variant="outline"
-					className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
-				>
-					Delete forever
-				</Button>
-			</ConfirmModal>
-		</div>
-	);
+  return (
+    <div className="flex items-center justify-center bg-rose-500 text-white w-full text-center text-sm p-2 gap-x-2">
+      <p>This note is in the trash</p>
+      <Button
+        size="sm"
+        onClick={onRestore}
+        variant="outline"
+        className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal">
+        Restore page
+      </Button>
+      <ConfirmModal onConfirm={onRemove}>
+        <Button
+          size="sm"
+          variant="outline"
+          className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal">
+          Delete forever
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
 };
