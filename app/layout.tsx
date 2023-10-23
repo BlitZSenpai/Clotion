59f1b2d7fc7a2b92f338371ceb11e7b059fe6106
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-10-23 18:17:58 +0530

    prettier config

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,48 +10,43 @@ import { EdgeStoreProvider } from "@/lib/edgestore";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Crux",
-	description: "Literally a Notion clone",
-	icons: {
-		icon: [
-			{
-				media: "(prefers-color-scheme: light)",
-				url: "/logo.svg",
-				href: "/logo.svg",
-			},
-			{
-				media: "(prefers-color-scheme: dark)",
-				url: "/logo-dark.svg",
-				href: "/logo-dark.svg",
-			},
-		],
-	},
+  title: "Crux",
+  description: "Literally a Notion clone",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg",
+      },
+    ],
+  },
 };
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body className={inter.className}>
-				<ConvexClientProvider>
-					<EdgeStoreProvider>
-						<ThemeProvider
-							attribute="class"
-							defaultTheme="system"
-							enableSystem
-							disableTransitionOnChange
-							storageKey="crux-theme"
-						>
-							<Toaster position="bottom-center" />
-							<ModalProvider />
-							{children}
-						</ThemeProvider>
-					</EdgeStoreProvider>
-				</ConvexClientProvider>
-			</body>
-		</html>
-	);
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="crux-theme">
+              <Toaster position="bottom-center" />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  );
 }
